420e0786ce6b753a8f863198365e1f6c3fa2fe38
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-08-19 15:52:22 -0700

    routes functions

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,15 +1,14 @@
 const router = require("express").Router();
-const store = require("db.json");
+const store = require("../db/store");
 
 // GET "/api/notes" responds with all notes from the database
-router.get("/notes", (req, res) => {
+router.get("/notes", function (req, res) {
   store
     .getNotes()
     .then((notes) => res.json(notes))
     .catch((err) => res.status(500).json(err));
 });
 
-/*
 router.post("/notes", (req, res) => {
   store
     .addNote(req.body)
@@ -17,12 +16,12 @@ router.post("/notes", (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete("/notes/:id", (req, res) => {
+// BONUS DELETE "/api/notes" deletes the note with an id equal to req.params.id
+router.delete("/notes/:id", function (req, res) {
   store
     .removeNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch((err) => res.status(500).json(err));
 });
-*/
+
 module.exports = router;
